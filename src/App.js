8df2b8594c7a41782712ef2e831ba08097cfde56
umbur <you@example.com>
2019-08-14 15:12:29 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
-
+ 
 const App = () => {
   const state = {
     additionalPrice: 0,
